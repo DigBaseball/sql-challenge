7a0eac42ba0995513617d775d5fed9de69c02286
DigBaseball <misha.berkowitz@gmail.com>
2020-04-21 08:48:49 -0700

    Additional work on queries.

--- Analysis/analysis.sql
+++ Analysis/analysis.sql
@@ -4,21 +4,55 @@
 -- 1. List the following details of each employee:
 -- employee number, last name, first name, gender, and salary.
 
-select employees.emp_no as "employee number",
-	employees.last_name as "last name",
-	employees.first_name as "first name",
-	employees.gender,
-	salaries.salary from salaries
-	LEFT JOIN employees
-	on employees.emp_no=salaries.emp_no;
+select e.emp_no as "employee number",
+	e.last_name as "last name",
+	e.first_name as "first name",
+	e.gender,
+	s.salary from salaries as s
+	LEFT JOIN employees as e
+	on e.emp_no=s.emp_no;
 
 
 -- 2. List employees who were hired in 1986
+
+select first_name as "first name",
+last_name as "last name"
+from employees
+where emp_no in
+(select emp_no
+from salaries
+where from_date between '1986-01-01' and '1986-12-31'
+order by from_date);
 
 
 -- 3. List the manager of each department with the following information:
 -- department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
 
+
+select d.dept_no as "department number",
+d.dept_name as "department name",
+dm.emp_no as "manager employee number",
+dm.from_date as "department start",
+dm.to_date as "department end",
+e.last_name as "last name",
+e.first_name as "first name",
+s.from_date as "start date",
+s.to_date as "end date"
+from departments as d
+join departmentmanagers as dm
+on dm.dept_no=d.dept_no
+join employees as e
+on e.emp_no = dm.emp_no
+join salaries as s
+on s.emp_no = e.emp_no;
+
+select dept_no, max(from_date) 
+from departmentmanagers
+group by dept_no
+order by dept_no
+
+select dept_no as dept_no, dept_name
+from departments
 
 -- 4. List the department of each employee with the following information:
 -- employee number, last name, first name, and department name.
